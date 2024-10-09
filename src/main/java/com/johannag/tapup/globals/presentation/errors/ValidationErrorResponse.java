d592e4cffe43032260c1766da069245b454224e5
Johanna <j.gendler.g@gmail.com>
2024-10-09 19:59:35 -0300

    fix

--- src/main/java/com/johannag/tapup/globals/presentation/errors/ValidationErrorResponse.java
+++ src/main/java/com/johannag/tapup/globals/presentation/errors/ValidationErrorResponse.java
@@ -1,12 +1,14 @@
 package com.johannag.tapup.globals.presentation.errors;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Value
+@EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 public class ValidationErrorResponse extends ErrorResponse {
     List<FieldError> fieldErrors;
